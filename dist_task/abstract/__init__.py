b581912ed929f5b490d2de327e69497f9489b771
wuruimiao <dev.wuruimiao@qq.com>
2025-02-28 10:52:35 +0800

    fix: import

--- dist_task/abstract/__init__.py
+++ dist_task/abstract/__init__.py
@@ -1,3 +1,3 @@
 from dist_task.abstract.proxy import Proxy
 from dist_task.abstract.task import Task
-from dist_task.abstract.worker import Worker, handler
+from dist_task.abstract.worker import Worker
